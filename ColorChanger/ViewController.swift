5cb7daf728641be981ef077871c4d878f1edbfa2
Eilyn Tudares <etuda001@fiu.edu>
2025-05-04 23:46:59 -0400

    Initial commit - Add Xcode project

--- ColorChanger/ViewController.swift
+++ ColorChanger/ViewController.swift
@@ -1,19 +1,28 @@
-//
-//  ViewController.swift
-//  ColorChanger
-//
-//  Created by Eilyn Fabiana Tudares Granadillo on 5/4/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    // Triggered when the button is tapped
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
 
+        // Optional: update label color for readability
+        titleLabel.textColor = .white
+    }
+
+    // Generates a random color
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
-
